67faea57703f374fb9da1a8b767fe5f4df30cd2a
hour-devel <nylihour001@gmail.com>
2024-09-23 16:23:27 +0700

    Initial Commit

---
+++ TouchID&FaceID/TouchID_FaceIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchID_FaceIDApp.swift
+//  TouchID&FaceID
+//
+//  Created by Lihour on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchID_FaceIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
